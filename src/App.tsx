da5bc794d8893bab99d3d6c08f47e8f071d666cf
ali hosseini <a75hosseini.js@gmail.com>
2025-12-31 18:56:46 +0330

    base layout sidebar created

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import { PanelLayout } from "./layouts/PanelLayout";
 import { Auth } from "./pages/auth/Auth";
 
 function App() {
   return (
     <>
-      <Auth />
+      {/* <Auth /> */}
+      <PanelLayout />
     </>
   );
 }
---
+++ src/layouts/PanelLayout.tsx
@@ -0,0 +1,12 @@
+import "./Panellayout.css";
+
+export const PanelLayout = () => {
+  return (
+    <>
+      <div className="panel-layout">
+        <div className="sideBar"></div>
+        <main className="main"></main>
+      </div>
+    </>
+  );
+};
